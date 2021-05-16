b3f3bf0273658b6140ffb0a366527c7e0e20daf4
cx <cxgit@b2sw.com>
2021-05-16 14:05:47 -0500

    ensure wait loop doesn't get optimized out; reduce screen tab size

--- ti994a_40col_codes.h
+++ ti994a_40col_codes.h
@@ -9,7 +9,7 @@
 void wait1s(void)
 {
     unsigned int retTime;
-    int r=5;
+    volatile int r=5;
     // Implement a dull loop!
     for(retTime=0; retTime<1000;++retTime){
         r*=5;
@@ -34,7 +34,7 @@ void init_term(void)
 void inputtxt(void) {}
 void evidence1(void) {++colc;printf("-");}
 void evidence2(void) {++colc;printf("*");}
-void tab(void) {printf("        ");}
+void tab(void) {printf("  ");}
 
 void leave(void) {}
 
